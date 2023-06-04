2aa814e9e2b1e3eb5020e8e9edebdc6bf6dea3c5
Monday_David_Sarka <david_md@hamari.agency>
2023-06-04 09:25:09 +0100

    Final Commit

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-alert("Hi Dave!");
+  const navigatorStatus = navigator.onLine;
+  if(navigatorStatus) {
+    document.getElementById("onlineStatus").innerHTML = `Welcome back <span>Online</span> Partner!!`;
+    document.getElementById("offlineStatus").style.display = "none";
+} else {
+    document.getElementById("offlineStatus").innerHTML = `This web Page is <span>Offline</span> Punk!!`;
+    document.getElementById("onlineStatus").style.display = "none";    
+  }
